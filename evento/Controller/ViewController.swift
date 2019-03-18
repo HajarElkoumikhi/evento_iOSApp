ab6e13c79dc8a43d28c4e9767c8b2eb6be50e013
Hajar <hajar.elkoumikhi@gmail.com>
2019-03-18 17:47:08 +0100

    Add project base files

---
+++ evento/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  evento
+//
+//  Created by Elitia Candy on 14/03/2019.
+//  Copyright © 2019 EventoTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

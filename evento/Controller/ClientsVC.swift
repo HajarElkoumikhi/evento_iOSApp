4f261c16cb9bb791abb9e7c75e2ddb2a25f8c1df
Hajar <hajar.elkoumikhi@gmail.com>
2019-03-21 17:00:45 +0100

    Update evento prototye

---
+++ evento/Controller/ClientsVC.swift
@@ -0,0 +1,14 @@
+//
+//  ClientsVC.swift
+//  evento
+//
+//  Created by Elitia Candy on 19/03/2019.
+//  Copyright © 2019 EventoTeam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ClientsVC: UIViewController {
+    
+}
---
+++ evento/Controller/MembersVC.swift
@@ -0,0 +1,14 @@
+//
+//  MembersVC.swift
+//  evento
+//
+//  Created by Elitia Candy on 19/03/2019.
+//  Copyright © 2019 EventoTeam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MembersVC: UIViewController {
+    
+}

49d57187357a06facab1dc2ba55ada17654b1cfe
Hajar <hajar.elkoumikhi@gmail.com>
2019-04-06 20:33:43 +0200

    Add project new structure

---
+++ evento_iOSApp/evento_iOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  evento_iOSApp
+//
+//  Created by Elitia Candy on 01/04/2019.
+//  Copyright © 2019 Elitia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

49ba5c2ea96203a73235cc950a9aab987b95120b
Hajar <hajar.elkoumikhi@gmail.com>
2019-05-02 14:13:57 +0200

    Update slide menu

---
+++ evento_iOSApp/evento_iOSApp/Addons/SWRevealMenu/BackMenuVC.swift
@@ -0,0 +1,34 @@
+//
+//  BackMenuVC.swift
+//  evento_iOSApp
+//
+//  Created by Elitia Candy on 30/04/2019.
+//  Copyright © 2019 EventoTeam. All rights reserved.
+//
+
+import Foundation
+
+class BackMenuVC: UITableViewController {
+    
+    var menuArray = [String]()
+    
+    override func viewDidLoad() {
+        menuArray = ["Profile", "Events","Clients","Users","About"]
+    }
+    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+//        return menuArray.count
+//    }
+
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell{
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath ) as UITableViewCell
+//        
+//        cell.textLabel?.text = menuArray[indexPath.row]
+//        
+//        return cell
+//    }
+    
+    
+
+    
+}
--- evento_iOSApp/evento_iOSApp/Controller/ViewController.swift
+++ evento_iOSApp/evento_iOSApp/Controller/ViewController.swift
@@ -2,72 +2,31 @@
 //  ViewController.swift
 //  evento_iOSApp
 //
-//  Created by EventoTeam on 08/04/2019.
+//  Created by Elitia Candy on 30/04/2019.
 //  Copyright © 2019 EventoTeam. All rights reserved.
 //
 
-//import UIKit
-//
-//class ViewController: UIViewController {
-//    
-//    @IBOutlet weak var tableView:UITableView?
-//    @IBOutlet weak var addButton: UIImage!
-//    
-//    var dataService = DataService.instance
-//    var authService = AuthService.instance
-//    
-//    
-//    override func viewDidLoad() {
-//        
-//        super.viewDidLoad()
-//        
-//        
-//        dataService.delegate = self
-//        tableView?.delegate = self
-//        tableView?.dataSource = self
-//        DataService.instance.getAllUsers()
-//    }
-//    
-//    
-//}
-//
-//
-//extension ViewController: DataServiceDelegate {
-//    func usersLoaded() {
-//        OperationQueue.main.addOperation {
-//            self.tableView?.reloadData()
-//        }
-//    }
-//
-//}
-//
-//
-//extension ViewController: UITableViewDelegate,UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return dataService.usersArray.count
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as? UserCell {
-//            cell.configureCell(user: dataService.usersArray[indexPath.row])
-//            
-//            return cell
-//        }
-//        else {
-//            return UITableViewCell()
-//        }
-//    }
-//    
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
-//    {
-//        return 200.0;//Custom row height
-//    }
-//    
-//    
-//}
-//
-//
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var open: UIBarButtonItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        open.target = self.revealViewController()
+        open.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+//        open.target(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.revealViewController().revealToggle(self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}

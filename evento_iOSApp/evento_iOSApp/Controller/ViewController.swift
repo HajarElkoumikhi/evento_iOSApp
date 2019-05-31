f0d8d8fa3039f214189aa28daa38b3eb39ddccfb
Hajar <hajar.elkoumikhi@gmail.com>
2019-05-31 15:29:29 +0200

    Revert "Side menu (#5)" (#6)
    This reverts commit 8b4c70344b8f29cb61dd0bff774ef7ed6315957f.

--- evento_iOSApp/evento_iOSApp/Controller/ViewController.swift
+++ evento_iOSApp/evento_iOSApp/Controller/ViewController.swift
@@ -2,29 +2,29 @@
 //  ViewController.swift
 //  evento_iOSApp
 //
-//  Created by Elitia Candy on 30/04/2019.
+//  Created by EventoTeam on 08/04/2019.
 //  Copyright © 2019 EventoTeam. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var open: UIBarButtonItem!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        open.target = self.revealViewController()
-        open.action = #selector(SWRevealViewController.revealToggle(_:))
-        
-        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        DataService.instance.delegate = self
+        DataService.instance.getAllUsers()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+
+
 }
+
+
+extension ViewController: DataServiceDelegate {
+    func usersLoaded() {
+        
+    }
+
+}
+
--- evento_iOSApp/evento_iOSApp/Services/DataService.swift
+++ evento_iOSApp/evento_iOSApp/Services/DataService.swift
@@ -9,132 +9,78 @@
 
 //parseUserData
 
-//import Foundation
+import Foundation
+
+protocol DataServiceDelegate: class {
+    func usersLoaded()
+}
+
+class DataService {
+    static let instance = DataService()
+    
+    weak var delegate: DataServiceDelegate?
+    var usersArray = [User]()
+    
+    //Get all food trucks
+    func getAllUsers(){
+        let sessionConfig = URLSessionConfiguration.default
+        
+        //Create session, and optionally set a URLSessionDelegate
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        // Create the request
+        // Get all users (Get /users)
+        guard let URL = URL(string: GET_ALL_USERS_URL) else {return}
+        var request = URLRequest(url: URL)
+        request.httpMethod = "GET"
+        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) -> Void in
+            if (error == nil){
+                //Success
+                let statusCode = (response as! HTTPURLResponse).statusCode
+                print("URL Session Task Succeeded: HTTP \(statusCode)")
+                
+                if let data = data {
+                    self.usersArray = User.parseUserData(data: data)
+                    self.delegate?.usersLoaded()
+                }
+            }
+            else {
+                // Failure
+                print("URL session failed \(error!.localizedDescription)")
+            }
+        })
+        task.resume()
+        session.finishTasksAndInvalidate()
+        
+    }
+        
+        
+
+    
+    
+    //POST add a new foodtruck
+//    func addNewFoodTruck(_ name:String, foodtype:String, avgCost: Double, latitude:Double, longitude:Double, completion:@escaping callbacks){
+//        //Construct our JSON
 //
-//protocol UserServiceDelegate: class {
-//    func usersLoaded()
-//    func eventsLoaded()
-//}
+//        let json: [String:Any] = [
+//            "name": name,
+//            "foodtype": foodtype,
+//            "avgcost": avgCost,
+//            "geometry":[
+//                "coordinates" :[
+//                    "lat": latitude,
+//                    "long": longitude
 //
-//class UserService {
-//    static let instance = UserService()
-//    
-//    weak var delegate: DataServiceDelegate?
-//    var usersArray = [User]()
-//    var eventsArray = [Event]()
-//    var clientsArray = [Client]()
-//    
-//    
-//    
-//    //Get all users
-//    func getAllUsers(){
-//        let sessionConfig = URLSessionConfiguration.default
-//        
-//        //Create session, and optionally set a URLSessionDelegate
-//        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-//        
-//        // Create the request
-//        // Get all users (Get /users)
-//        guard let URL = URL(string: GET_ALL_USERS_URL) else {return}
-//        var request = URLRequest(url: URL)
-//        request.httpMethod = "GET"
-//        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            if (error == nil){
-//                //Success
-//                let statusCode = (response as! HTTPURLResponse).statusCode
-//                print("URL Session Task Succeeded: HTTP \(statusCode)")
-//                
-//                if let data = data {
-//                    self.usersArray = User.parseUserData(data: data)
-//                    self.delegate?.usersLoaded()
-//                }
-//            }
-//            else {
-//                // Failure
-//                print("URL session failed \(error!.localizedDescription)")
-//            }
-//        })
-//        task.resume()
-//        session.finishTasksAndInvalidate()
-//        
+//                ]
+//            ]
+//        ]
+//
+//        //        do {
+//        //            //Serialize JSON
+//        //            let jsonData = try JSONSerialization.data(withJSONObject: JSONSerialization, options: .prettyPrinted )
+//        //        }catch let
+//
 //    }
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    //Get all events
-//    func getAllEvents(){
-//        let sessionConfig = URLSessionConfiguration.default
-//        
-//        //Create session, and optionally set a URLSessionDelegate
-//        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-//        
-//        // Create the request
-//        // Get all events (Get /events)
-//        guard let URL = URL(string: GET_ALL_EVENTS_URL) else {return}
-//        var request = URLRequest(url: URL)
-//        request.httpMethod = "GET"
-//        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            if (error == nil){
-//                //Success
-//                let statusCode = (response as! HTTPURLResponse).statusCode
-//                print("URL Session Task Succeeded: HTTP \(statusCode)")
-//                
-//                if let data = data {
-//                    self.eventsArray = Event.parseEventData(data: data)
-//                    self.delegate?.usersLoaded()
-//                }
-//            }
-//            else {
-//                // Failure
-//                print("URL session failed \(error!.localizedDescription)")
-//            }
-//        })
-//        task.resume()
-//        session.finishTasksAndInvalidate()
-//        
-//    }
-//
-//
-//    
-//    
-//    
-//    //Get all clients
-//    func getAllClients(){
-//        let sessionConfig = URLSessionConfiguration.default
-//        
-//        //Create session, and optionally set a URLSessionDelegate
-//        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-//        
-//        // Create the request
-//        // Get all clients (Get /clients)
-//        guard let URL = URL(string: GET_ALL_CLIENTS_URL) else {return}
-//        var request = URLRequest(url: URL)
-//        request.httpMethod = "GET"
-//        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            if (error == nil){
-//                //Success
-//                let statusCode = (response as! HTTPURLResponse).statusCode
-//                print("URL Session Task Succeeded: HTTP \(statusCode)")
-//                
-//                if let data = data {
-//                    self.clientsArray = Client.parseClientData(data: data)
-//                    self.delegate?.usersLoaded()
-//                }
-//            }
-//            else {
-//                // Failure
-//                print("URL session failed \(error!.localizedDescription)")
-//            }
-//        })
-//        task.resume()
-//        session.finishTasksAndInvalidate()
-//        
-//    }
-//    
-//
-//
-//}
+
+
+}

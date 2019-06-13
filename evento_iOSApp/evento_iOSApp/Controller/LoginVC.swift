2d4f942b00a56a1e2d82cd653c87a986cc75e6aa
Hajar <hajar.elkoumikhi@gmail.com>
2019-06-13 16:34:52 +0200

    Update login feature

--- evento_iOSApp/evento_iOSApp/Controller/LoginVC.swift
+++ evento_iOSApp/evento_iOSApp/Controller/LoginVC.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class LoginVC: UIViewController {
     
-    
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
@@ -18,7 +17,6 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 
@@ -78,38 +76,5 @@ class LoginVC: UIViewController {
         present(alertController,animated: true,completion: nil)
         
     }
-    
-//    func getAllAdmins(){
-//        let sessionConfig = URLSessionConfiguration.default
-//
-//        //Create session, and optionally set a URLSessionDelegate
-//        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-//
-//        // Create the request
-//        // Get all admins (Get /events)
-//        guard let URL = URL(string: GET_ALL_EVENTS_URL) else {return}
-//        var request = URLRequest(url: URL)
-//        request.httpMethod = "GET"
-//        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            if (error == nil){
-//                //Success
-//                let statusCode = (response as! HTTPURLResponse).statusCode
-//                print("URL Session Task Succeeded: HTTP \(statusCode)")
-//
-//                if let data = data {
-//                    self.eventsArray = Event.parseEventData(data: data)
-//                    self.delegate?.eventsLoaded()
-//                }
-//            }
-//            else {
-//                // Failure
-//                print("URL session failed \(error!.localizedDescription)")
-//            }
-//        })
-//        task.resume()
-//        session.finishTasksAndInvalidate()
-//
-//    }
-
 
 }
